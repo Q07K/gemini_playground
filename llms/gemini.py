a58a9aed5121b73720f43c7b601b944760bd86fb
gyuhyeong <kgh0730@gmail.com>
2025-02-27 02:35:42 +0900

    Feat: Gemini 사용을 위한 기능 개발
    - get_model_list: 모델 리스트 추출 함수
    - valid_api_key: API KEY 사용 가능 확인 함수
    - generate: 텍스트 생성 함수

---
+++ llms/gemini.py
@@ -0,0 +1,40 @@
+import os
+
+import google.generativeai as genai
+from google.auth.exceptions import DefaultCredentialsError
+
+
+def get_model_list():
+    return [
+        model.name
+        for model in genai.list_models()
+        if "generateContent" in model.supported_generation_methods
+    ]
+
+
+def valid_api_key():
+    try:
+        genai.configure(api_key=os.getenv("api_key"))
+        genai.list_models()
+        return True
+    except DefaultCredentialsError:
+        return False
+
+
+def generate(
+    model_name: str,
+    system_prompt: str,
+    document: str,
+    query: str,
+):
+    model = genai.GenerativeModel(
+        model_name=model_name, system_instruction=system_prompt
+    )
+    agent = model.start_chat(
+        history=[
+            {"role": "user", "parts": [document]},
+        ]
+    )
+    for chunk in agent.send_message(content=query, stream=True):
+        if chunk.text:
+            yield chunk.text
